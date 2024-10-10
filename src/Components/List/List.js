528b94da5e1be66c6961faf8d85e5e42cbbf9adb
sanskriti1209 <139151589+sanskriti1209@users.noreply.github.com>
2024-10-10 13:02:21 +0530

    Update List.js

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -62,7 +62,7 @@ export default function List(props) {
                             }[props.groupValue]
                         }
                     </div>
-                    
+                    <div className="list-sum">{cardCount}</div>
                 </div>
                 <div className="list-header-right">
                     <div className="list-add-item">
@@ -72,6 +72,7 @@ export default function List(props) {
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path fill="currentColor" d="M10.001 7.8a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 10 7.8zm-7 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 3 7.8zm14 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 17 7.8z"/></svg>
                     </div>
                 </div>
+                      
             </div>
 
             <div className="list-card-items">
